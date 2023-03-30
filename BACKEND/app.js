7bd024d01185429a3d2700bb15b3ca1cd366a634
Sushil <oct9sushil@gmail.com>
2023-03-30 12:54:41 +0545

    crud apis for users

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const userRouter = require("./routes/users");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const PORT = 4000;
+const app = express();
+app.use(express.json()); //Middleware to parse json body received in request.
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.status(200).send({ status: "Server is running" });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+app.use("/api/users", userRouter);
+
+// app.get('/api/greet', (req,res)=>{
+//     res.send({msg:'Hello Everyone'})
+// })
+
+// app.get('api/greet/:name',(req,res)=>{
+//     const name = req.params.name;
+//     res.send({msg:`Hello ${name}! Good Morning`})
+// })
+
+// app.post('api/courses',(req,res)=>{
+//     const course =req.body;
+//     res.send(course);
+// })
+
+// app.get('api//search',(req,res)=>{
+//     const query =request.query
+//     console.log(query.id)
+// })
+
+app.listen(PORT, () => console.log(`Express app runniing on ${PORT}`));
--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var users = [
+  {
+    id: 1,
+    name: "sushil",
+  },
+  {
+    id: 2,
+    name: "Bimala",
+  },
+];
+
+router.get("/", (req, res) => {
+  const query = req.query;
+  if (query.id) {
+    const user = users.find((user) => user.id == query.id);
+    if (user) res.send(user);
+    else res.status(404).send({ error: `User with id ${query.id} not found` });
+  } else {
+    res.send(users);
+  }
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send({
+    msg: "User added sucessfully",
+    user: user,
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const userId = req.params.id;
+  const userName = req.body.name;
+  const user = users.find((user) => user.id == userId);
+  if (user) {
+    user.name = userName;
+    res.send({ msg: "User has been updated", user: user });
+  } else res.status(404).send({ error: `User with id ${userId} not found` });
+});
+
+router.delete("/:id", (req, res) => {
+  const userId = req.params.id;
+  const user = users.find((user) => user.id == userId);
+  if (user) {
+    users = users.filter(user => user.id != userId);
+    res.send({ msg: "User deleted sucessfully" });
+  } else res.status(404).send({ error: `User with id ${userId} not found` });
 });
 
 module.exports = router;
